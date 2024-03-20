4df1d24b01df140050fcf0299a5b8f4e57b23d43
aguo312 <andrew.guo@stonybrook.edu>
2024-03-20 18:43:46 -0400

    initial client/server connection

---
+++ server/src/main/java/com/DiscussionForum/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.DiscussionForum.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        System.out.println("Hey");
+        return "This is a message from the server.";
+    }
+
+}
---
+++ server/src/main/java/com/DiscussionForum/model/User.java
@@ -0,0 +1,76 @@
+package com.DiscussionForum.model;
+
+import java.time.LocalDate;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Users")
+public class User {
+
+    @Id
+    private ObjectId id;
+    private String username;
+    private String email;
+    private String password;
+    private int reputation;
+    private LocalDate timeCreated;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.reputation = 0;
+        this.timeCreated = LocalDate.now();
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getReputation() {
+        return reputation;
+    }
+
+    public void setReputation(int reputation) {
+        this.reputation = reputation;
+    }
+
+    public LocalDate getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(LocalDate timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+}
