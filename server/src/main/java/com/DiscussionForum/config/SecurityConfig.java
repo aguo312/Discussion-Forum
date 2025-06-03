38758453b551c6593041a5a4de30b7f252124d94
aguo312 <andrewguo312@gmail.com>
2025-06-03 09:41:03 -0400

    Added Password Encoder

--- server/src/main/java/com/DiscussionForum/config/SecurityConfig.java
+++ server/src/main/java/com/DiscussionForum/config/SecurityConfig.java
@@ -10,7 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -32,9 +34,14 @@ public class SecurityConfig {
     }
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(passwordEncoder());
         provider.setUserDetailsService(userDetailsService);
         return provider;
     }
--- server/src/main/java/com/DiscussionForum/service/UserService.java
+++ server/src/main/java/com/DiscussionForum/service/UserService.java
@@ -1,6 +1,7 @@
 package com.DiscussionForum.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.DiscussionForum.model.User;
@@ -12,12 +13,15 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
     public User newUser(String username, String email, String password) {
-        return userRepository.save(new User(username, email, password));
+        return userRepository.save(new User(username, email, passwordEncoder.encode(password)));
     }
 
     public User verifyPassword(String email, String password) {
